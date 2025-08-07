a828379986aeee27f7818aaef341b4de94d74773
Umid Yuldashev <yumid253@gmail.com>
2025-08-07 11:06:27 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import bodyParser from "body-parser";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express()
+const port = 3000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(express.static(__dirname + "/public/"))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/main.html")
+})
+
+app.listen(port, () => console.log("http://localhost:" + port))
